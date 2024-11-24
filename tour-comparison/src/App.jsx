3ef73c1b7b542e8ffa44354d08b532a337a62241
Martin Leone <martinnikolaslll@gmail.com>
2024-11-24 13:30:44 -0500

    fixing useState error

--- tour-comparison/src/App.jsx
+++ tour-comparison/src/App.jsx
@@ -1,10 +1,17 @@
 //Task 1: App.jsx (Root Component)
 
 
-import React from "react";
+import React, { useState } from "react";
 import Gallery from './Gallery';
 
 const App = () => {
+  const [currentPage, setCurrentPage] = useState('gallery');
+
+  const navigate = (page) => {
+    setCurrentPage(page);
+  }
+
+
 //route structure
   return (
     <div>
